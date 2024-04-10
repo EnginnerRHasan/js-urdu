e5c4fc41997d6b1bf684730c74898e01d0082c3b
EnginnerRHasan <163845006+EnginnerRHasan@users.noreply.github.com>
2024-04-10 04:49:15 +0000

    variables explain

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14455
+let accountEmail = "rafayim12@gmail.com"
+var accountPassword = "121212"
+accountCity = "Karachi"
+let accountstate;
+
+// accountId = 2 // not allowed redifine
+
+accountEmail = "gg@gmail.com"
+accountPassword = "12345"
+accountCity = "lahore"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
